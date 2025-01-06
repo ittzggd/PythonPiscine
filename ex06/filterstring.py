52a8880c081797a7efcec5459d3f70d942f53624
Hee Ju Jang <01kathyy@gmail.com>
2025-01-06 20:49:15 +0900

    fix: norminette

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -2,14 +2,6 @@ from ft_filter import ft_filter
 import sys
 
 
-# def string_validation(input) -> bool:
-#     """
-#     Checks a validation of string from a command-line argument
-#     """
-#     if any(not input.isdigit() and not input.isalpha() and not input.isspace()):
-        
-
-
 def execute_filter():
     """
     Filters a list of words based on their length.
@@ -26,7 +18,7 @@ def execute_filter():
         raise AssertionError(": the arguments are bad")
     if not all(char.isalnum() or char.isspace() for char in sys.argv[1]):
         raise AssertionError(": invalid character")
-    
+
     words = list(sys.argv[1].split(' '))
     result = ft_filter(lambda x: len(x) > int(sys.argv[2]), words)
     printed_res = [res for res in result]
